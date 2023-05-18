4d6995cc71315e2b83de6ce8e379969230f0027b
Adnan <adnan11odimah@gmail.com>
2023-05-18 22:26:46 +0100

    Set up general File structure

---
+++ __init__.py
@@ -0,0 +1,4 @@
+""" Initialiser for Project Athena.
+__version__ returns version """
+
+__version__ = (0.1, "Alpha")
---
+++ __main__.py
@@ -0,0 +1,6 @@
+""" Main File with Script for Athena """
+
+# from _ import _
+
+if __name__ == "__main__":
+    pass
